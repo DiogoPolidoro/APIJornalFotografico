b9947d6c9fd24176ac9c6f8978041972e803749b
DPolidoro <polidorodiogo@gmail.com>
2024-12-30 00:00:26 +0000

    API

--- src/main/java/com/example/APIjornalfotografico/repositories/UserRepo.java
+++ src/main/java/com/example/APIjornalfotografico/repositories/UserRepo.java
@@ -4,6 +4,8 @@ import com.example.APIjornalfotografico.models.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<Users, Long> {
+import java.util.Optional;
 
+public interface UserRepo extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
 }
